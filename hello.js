0415307f626d79cee3f6d45763bc192df9f6aa20
Kelly Murray <kellymurray@users.noreply.github.com>
2017-08-21 19:34:39 -0400

    integrate api call to wanikani crit items

--- hello.js
+++ hello.js
@@ -1,21 +1,26 @@
-'use strict';
-
-const request = require('request');
-
-const baseURL = "https://www.wanikani.com/api/user/";
-let url = baseURL+ "b519f17cd3b964e8fe3ba0f999ab318c/critical-items/95";
-console.log(url);
-
-const callAPI = (url) => {
-  request.get(url) {
-    console.log(url);
-  });
-};
-
-callAPI();
+var request = require('request');
 
 module.exports = function(ctx, cb) {
 
+  var baseURL = "https://www.wanikani.com/api/user/";
+  var apiKey = ctx.secrets.wanikani_api_key;
+  var url = baseURL + apiKey + "/critical-items/95";
+  console.log(url);
+  
+  request.get(url, function (error, res, body) {
+    if (error) {
+      console.log(error);
+      cb(null, error);
+    } else {
+      var result = JSON.parse(body);
+      console.log(result.requested_information[0].character);
+    }
+ 
+  cb(null, {text: "こんにちわ! Your top critical word today is " + result.requested_information[0].character + ". It means _" + result.requested_information[0].meaning + "_."
+  
+    });
 
+  });
 
-}
+};
+
